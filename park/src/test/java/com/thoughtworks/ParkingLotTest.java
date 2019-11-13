09eba464a0c024a02b739f240306786fef935da8
Pengbo Yang <pengbo.yang@PengbodeMacBook-Pro.local>
2019-11-13 15:20:32 +0800

    add give when then commet

--- park/src/test/java/com/thoughtworks/ParkingLotTest.java
+++ park/src/test/java/com/thoughtworks/ParkingLotTest.java
@@ -7,50 +7,63 @@ public class ParkingLotTest {
 
     @Test
     public void given1SpaceParkLotAndCarWhenParkingThenGetTicket() {
+        //give
         ParkLot parkLot = new ParkLot(1);
         Car car = new Car();
+        //when
         Ticket ticket = parkLot.Park(car);
+        //then
         Assert.assertNotNull(ticket);
         Assert.assertEquals(car.getNumber(), ticket.getCarNumber());
     }
 
     @Test
     public void giveNoSpaceParkLotAndCarWhenParkingThenFailure() {
+        //give
         ParkLot parkLot = new ParkLot(1);
         Car car1 = new Car();
         parkLot.Park(car1);
-
         Car car2 = new Car();
+        //when
         Ticket ticket = parkLot.Park(car2);
+        //then
         Assert.assertNull(ticket);
 
     }
 
     @Test
     public void giveParkLotAndTicketWhenPickupThenGetCar() {
+        //give
         ParkLot parkLot = new ParkLot(1);
         Car car = new Car();
         Ticket ticket = parkLot.Park(car);
-
+        //when
         Car pickedCar = parkLot.pickUp(ticket);
+        //then
         Assert.assertTrue(pickedCar == car);
 
     }
 
     @Test
     public void giveParkLotAndNullTicketWhenPickupThenGetNull() {
+        //give
         ParkLot parkLot = new ParkLot(1);
+        //when
         Car car = parkLot.pickUp(null);
+        //then
         Assert.assertNull(car);
 
     }
 
     @Test
     public void giveParkLotAndInvalidTicketWhenPickupThenGetNull() {
+        //give
         ParkLot parkLot = new ParkLot(1);
         Ticket ticket = parkLot.Park(new Car());
         parkLot.pickUp(ticket);
+        //when
         Car secondPickup = parkLot.pickUp(ticket);
+        //then
         Assert.assertNull(secondPickup);
 
     }
