07c0f298add437c20285d8423bcbd19f6a9156ae
Pengbo Yang <pengbo.yang@PengbodeMacBook-Pro.local>
2019-11-13 15:00:29 +0800

    Add nospace case

--- park/src/main/java/com/thoughtworks/ParkLot.java
+++ park/src/main/java/com/thoughtworks/ParkLot.java
@@ -5,12 +5,23 @@ import java.util.Map;
 
 public class ParkLot {
 
-    private Map<Ticket,Car> parkDatas=new HashMap<>();
-    
-	public Ticket Park(Car car) {
-        Ticket ticket= new Ticket(car.getNumber());
-        parkDatas.put(ticket, car);
-        return ticket;
-	}
+    private Map<Ticket, Car> parkDatas = new HashMap<>();
+    private int totalSpaceCount;
+
+    public ParkLot(int totalSpaceCount) {
+        super();
+        this.totalSpaceCount = totalSpaceCount;
+    }
+
+    public Ticket Park(Car car) {
+        if(parkDatas.size()<totalSpaceCount){
+            Ticket ticket = new Ticket(car.getNumber());
+            parkDatas.put(ticket, car);
+            return ticket;
+        }else{
+            return null;
+        }
+
+    }
 
 }
--- park/src/test/java/com/thoughtworks/ParkingLotTest.java
+++ park/src/test/java/com/thoughtworks/ParkingLotTest.java
@@ -3,15 +3,26 @@ package com.thoughtworks;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ParkingLotTest{
+public class ParkingLotTest {
 
     @Test
-    public void givenEmptyParkLotAndCarWhenParkingThenGetTicket(){
-        ParkLot parkLot=new ParkLot();
-        Car car=new Car();
-        Ticket ticket= parkLot.Park(car);
+    public void given1SpaceParkLotAndCarWhenParkingThenGetTicket() {
+        ParkLot parkLot = new ParkLot(1);
+        Car car = new Car();
+        Ticket ticket = parkLot.Park(car);
         Assert.assertNotNull(ticket);
         Assert.assertEquals(car.getNumber(), ticket.getCarNumber());
+    }
+
+    @Test
+    public void giveNoSpaceParkLotAndCarWhenParkingThenFailure() {
+        ParkLot parkLot = new ParkLot(1);
+        Car car1 = new Car();
+        parkLot.Park(car1);
+
+        Car car2 = new Car();
+        Ticket ticket = parkLot.Park(car2);
+        Assert.assertNull(ticket);
 
     }
 
